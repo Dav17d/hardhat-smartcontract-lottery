8dcaf76cb80090df2f26c9af8f25f5ad3f3ed222
Dav17d <75180393+Dav17d@users.noreply.github.com>
2022-07-18 19:15:11 +0500

    Initial commit

---
+++ helper-hardhat-config.js
@@ -0,0 +1,25 @@
+const networkConfig = {
+    4: {
+        name: "rinkeby",
+        subscriptionId: "8724",
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        interval: "30",
+        entranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: "100000000000000000", // 0.1 eth
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+        callbackGasLimit: "500000",
+        interval: "30",
+    },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+}
